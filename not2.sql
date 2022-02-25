bdb9138d6ea7b178b5d7cd965b14a903562b2361
Michal Badura <mbadura@interia.eu>
2022-02-25 21:24:54 +0100

    zadania coalesce

--- not2.sql
+++ not2.sql
@@ -16,3 +16,26 @@ where r.id_rating = 3;
 select * from rating_analytics ra
 inner join rating r on ra.rating = r.rating
 where r.id_rating in (3,2,5);
+
+select * from tasks1_7.school;
+select * from tasks1_7.class;
+select * from tasks1_7.child;
+
+select * from tasks1_7.child ch
+inner join tasks1_7.class cl on ch.class_id = cl.class_id
+inner join tasks1_7.school s on s.school_id = cl.school_id
+where s.school_id = 1;
+
+
+delete
+from tasks1_7.school
+where school_id = 1;
+
+
+SELECT
+    UNIQUE_CONSTRAINT_SCHEMA,
+    TABLE_NAME,
+    REFERENCED_TABLE_NAME
+FROM
+    information_schema.referential_constraints
+WHERE delete_rule = 'CASCADE' and UNIQUE_CONSTRAINT_SCHEMA = 'tasks1_7';
