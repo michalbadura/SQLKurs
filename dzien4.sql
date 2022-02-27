419fb1163564331dd6d00b6f6884fc3849e64b52
Michal Badura <mbadura@interia.eu>
2022-02-27 14:13:47 +0100

    nastepne

--- dzien4.sql
+++ dzien4.sql
@@ -33,3 +33,30 @@ with payment_types as (
     from payment
     group by payment_type
 ) select total_amount, payment_type, total_amount / sum(total_amount) over () from payment_types;
+
+
+
+create procedure film_classification(in length int)
+begin
+    if length < 60 then select 'very short';
+    elseif length between 60 and 90 then select 'short';
+    elseif length between 90 and 120 then select 'normal';
+    elseif length between 120 and 150 then select 'long';
+    else select 'very long';
+    end if;
+end;
+
+call film_classification(300);
+
+
+
+create procedure film_classification (in length integer)
+begin
+    case
+        when length < 60 then select 'very short';
+        when length between 60 and 90 then select 'short';
+        when length > 90 then select 'long';
+        else select 'ERROR';
+        end case;
+end;
+call film_classification(70);
