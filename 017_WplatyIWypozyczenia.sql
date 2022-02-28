94d7f42129ec5de413b9c117df5ad09c0d8013f7
Michal Badura <mbadura@interia.eu>
2022-02-28 23:15:42 +0100

    wyplata wypozyczen

--- 017_WplatyIWypozyczenia.sql
+++ 017_WplatyIWypozyczenia.sql
@@ -13,10 +13,23 @@ Pamiętaj o użyciu aliasów dla tabel w zapytaniu.
 To i następne 2 zadania będą składowymi do stworzenia naszego pierwszego raportu wypożyczeń.
 
 Uwaga
-Jeśli w dwóch tabelach jest ta sama nazwa kolumny, SQL będzie wymagał podania, z której tabeli chcemy wyświetlić kolumnę (można używać aliasów), przykładowo:
+Jeśli w dwóch tabelach jest ta sama nazwa kolumny, SQL będzie wymagał podania, z której tabeli chcemy wyświetlić kolumnę (można używać aliasów),
+ przykładowo:
 
 SELECT
     t.col_name
 FROM tab as t
  */
 
+
+
+
+SELECT
+       payment_id,
+       p.rental_id,
+       amount,
+       rental_date,
+       payment_date
+FROM sakila3_7.payment p
+INNER JOIN sakila3_7.rental r
+ON p.rental_id = r.rental_id;
