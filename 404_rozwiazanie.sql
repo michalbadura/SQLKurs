88a8ff6a4c1ebe858d0b4e621e65499578dae265
Michal Badura <mbadura@interia.eu>
2022-03-13 22:40:57 +0100

    rozwiazanie 2/3

--- 404_rozwiazanie.sql
+++ 404_rozwiazanie.sql
@@ -3,3 +3,10 @@ select e.gender, avg(salary) from salaries s
     where now() between from_date and to_date
 group by e.gender;
 
+select e.gender, d.dept_name, avg(salary), count(*) from salaries s
+    inner join employees e on s.emp_no = e.emp_no
+    inner join dept_emp de on s.emp_no = de.emp_no
+    inner join departments d on de.dept_no = d.dept_no
+    where now() between s.from_date and s.to_date
+group by e.gender, d.dept_name with rollup
+    order by dept_name;
