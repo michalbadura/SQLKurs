3059eeb084c4b0c7991a95bf61edf1a57fe14ec2
Michal Badura <mbadura@interia.eu>
2022-03-24 23:56:03 +0100

    liczba aktorow w filmie zrobione

--- 028_LiczbaAktorowWFilmie.sql
+++ 028_LiczbaAktorowWFilmie.sql
@@ -8,11 +8,10 @@ Wyniki zapisz do tabeli tymczasowej, np. tmp_film_actors.
 Dodatkowo napisz zapytanie, którym zweryfikujesz swoją kwerendę.
  */
 
-
-
-SELECT title AS "nazwa filmu",
-       COUNT(fa.actor_id) AS "liczb aktorów"
+CREATE VIEW tmp_film_actors
+    AS SELECT title AS "nazwa filmu",
+              COUNT(fa.actor_id) AS "liczb aktorów"
 FROM sakila3_7.film AS f
-INNER  JOIN sakila3_7.film_actor AS fa
-ON f.film_id = fa.film_id
+    INNER  JOIN sakila3_7.film_actor AS fa
+        ON f.film_id = fa.film_id
 GROUP BY `nazwa filmu`;
