f38fe655745ee4ac8655a8e0d2b2303905516370
Michal Badura <mbadura@interia.eu>
2022-03-24 15:39:55 +0100

    liczba aktorow w filmie

---
+++ 028_LiczbaAktorowWFilmie.sql
@@ -0,0 +1,18 @@
+/*
+Napisz kwerendę, która zwróci następujące informacje:
+
+nazwę filmu,
+liczbę aktorów występujących w filmie.
+Wyniki zapisz do tabeli tymczasowej, np. tmp_film_actors.
+
+Dodatkowo napisz zapytanie, którym zweryfikujesz swoją kwerendę.
+ */
+
+
+
+SELECT title AS "nazwa filmu",
+       COUNT(fa.actor_id) AS "liczb aktorów"
+FROM sakila3_7.film AS f
+INNER  JOIN sakila3_7.film_actor AS fa
+ON f.film_id = fa.film_id
+GROUP BY `nazwa filmu`;
