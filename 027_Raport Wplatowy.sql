53ea508573c0ef934cccda3809e0d5eac80d88c7
Michal Badura <mbadura@interia.eu>
2022-03-24 14:55:18 +0100

    raport wplatowy 2

--- 027_Raport Wplatowy.sql
+++ 027_Raport Wplatowy.sql
@@ -13,26 +13,27 @@ Wynik zapytania zapisz w bazie używając widoku.
 Jak sprawdzisz, czy Twoje zapytanie jest poprawne? Napisz odpowiedną kwerendę(y).
  */
 
-SELECT * FROM sakila3_7.customer;
 
-SELECT * FROM sakila3_7.payment;
-
+USE sakila3_7           -- USE dodane jako rozwiazanie bledu msql 1046
+CREATE VIEW customer_payments AS
 SELECT c.first_name AS "imię klienta",
        c.last_name AS "nazwisko klienta",
        c.email "email klienta",
-       SUM(p.amount) AS "kwota wpłat"
-
+       SUM(p.amount) AS "kwota wpłat",
+       COUNT(p.amount) AS "liczbę wpłat",           -- wydaje mi sie, ze mozna rowniez uzyc COUNT(p.customer_id)
+       AVG(p.amount) AS "średnią kwotę wpłat",
+       MAX(p.payment_date) AS "data ostatniej wpłaty"
 FROM sakila3_7.customer AS c
 INNER JOIN sakila3_7.payment AS p
 ON c.customer_id = p.customer_id
 GROUP BY c.customer_id;
 
 
-SELECT customer_id AS client,
-       date_format(payment_date, '%Y-%m') AS month,
-       SUM(amount) AS monthly_income_per_client
-FROM sakila3_7.payment
-GROUP BY client, month
-ORDER BY client, monthly_income_per_client DESC;
 
+-- SPRAWDZENIE
 
+select sum("kwota wpłat") from customer_payments
+having sum("kwota wpłat") <> (select sum(amount) from payment);
+
+select sum("liczbę wpłat") from customer_payments
+having sum("liczbę wpłat") <> (select count(*) from payment);
