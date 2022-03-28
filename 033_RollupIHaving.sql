c541a9432687b82f94874c065339b8cda3193ff6
Michal Badura <mbadura@interia.eu>
2022-03-28 10:18:42 +0200

    zadanie 33

--- 033_RollupIHaving.sql
+++ 033_RollupIHaving.sql
@@ -4,9 +4,11 @@ Na podstawie tabeli payment napisz zapytanie, które:
 wyznaczy sumę wpłat w podziale na klienta oraz pracownika,
 wyznaczy sumę wpłat per klient,
 wyznaczy sumę wpłat.
+
 Wykonaj dwie wersje tego zadania:
 
 Używając tylko ROLLUP,
+
 Dodając w GROUP BY klauzulę HAVING dla płatności większych od 70.
 Jak różnią się wyniki zapytań? Aby było łatwiej zauważyć różnicę, zadanie wykonaj tylko dla pierwszych 4 customer (customer_id < 4).
 
@@ -20,3 +22,27 @@ Oczekiwany wynik zapytania (poglądowy)
 |NULL       |NULL    |383.15     |
 +-----------+--------+-----------+
  */
+
+SELECT * FROM sakila3_7.payment;
+
+
+SELECT p.customer_id,
+       p.staff_id,
+       SUM(p.amount) AS "payments"
+FROM sakila3_7.payment p
+WHERE p.customer_id < 4
+GROUP BY p.staff_id, p.customer_id
+WITH ROLLUP;
+
+
+SELECT p.customer_id,
+       p.staff_id,
+       SUM(p.amount) AS "payments"
+FROM sakila3_7.payment p
+WHERE customer_id < 4
+GROUP BY p.staff_id, p.customer_id
+WITH ROLLUP
+HAVING payments > 70;
+
+
+
