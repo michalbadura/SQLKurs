fbd8c3cc5ede6974448ff3114d5714597c4a5352
Michal Badura <mbadura@interia.eu>
2022-04-04 23:49:41 +0200

    nowe zadanie

---
+++ 042_KwotaWplaty.sql
@@ -0,0 +1,14 @@
+/*
+Napisz zapytanie, które zgrupuje wpłaty według następującej klasyfikacji:
+
+fee - wpłaty poniżej 2,
+regular - w przeciwnym wypadku.
+
+W celu wykonania zadania użyj tabeli payment.
+
+Wynik pogrupuj i używając SQL, odpowiedz na pytanie, jaki procent wszystkich wpłat stanowią kary (fee).
+ */
+
+SELECT * FROM sakila3_7.payment;
+
+
