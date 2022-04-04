459037a2075cadf67ac8c08aa3ed7b0af27b0981
Michal Badura <mbadura@interia.eu>
2022-04-04 22:08:31 +0200

    nowe zadanie

---
+++ 041_DlugoscFilmu.sql
@@ -0,0 +1,12 @@
+/*
+Napisz procedurę, która na podstawie długości trwania filmu (np. kolumna length z film) przypisze rekord do jednej z poniższych grup:
+
+very short - film do 1h,
+short - film do 1.5h,
+normal - film do 2h,
+long - film do 2.5h,
+very long - film ponad 2.5h.
+
+Procedurę nazwij film_classification.
+
+ */
