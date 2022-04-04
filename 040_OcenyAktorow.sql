cea94198c1486347ecd71052b7ccc37ab7fee479
Michal Badura <mbadura@interia.eu>
2022-04-04 19:57:19 +0200

    40,5

---
+++ 040_OcenyAktorow.sql
@@ -0,0 +1,37 @@
+/*
+Używając tabeli actor_analytics, napisz zapytanie, które pogrupuje aktorów według poniższych kryteriów:
+
+jeśli avg_film_rate < 2 - 'poor acting',
+jeśli avg_film_rate jest pomiędzy 2 oraz 2.5 - 'fair acting',
+jeśli avg_film_rate jest pomiędzy 2.5 oraz 3.5 - 'good acting',
+jeśli avg_film_rate jest powyżej 3.5 - 'superb acting.
+
+Tak stworzoną kolumnę nazwij acting_level i następnie na jej podstawie dokonaj następującej analizy, obliczając:
+
+liczbę wystąpień w każdej grupie,
+sumę przychodów każdej grupy,
+liczbę filmów w każdej grupie,
+średni rating w grupie.
+Wskazówka
+Zadanie wykonaj w dwóch krokach:
+
+Stwórz podzapytanie, w którym stworzona zostanie kolumna acting_level,
+Na podstawie wyników z poprzedniego podpunktu wykonaj resztę zadania.
+
+ */
+
+SELECT *
+FROM sakila3_7.actor_analytics;
+
+
+
+SELECT
+       *,
+       CASE
+            WHEN avg_film_rate < 2 THEN 'poor acting'
+            WHEN avg_film_rate BETWEEN 2 AND 2.5 THEN 'fair acting'
+            WHEN avg_film_rate BETWEEN 2.5 AND 3.5 THEN 'good acting'
+            WHEN avg_film_rate > 3.5 THEN 'suberb acting'
+            ELSE 'error'
+       END AS acting_level
+FROM sakila3_7.actor_analytics;
--- 045_WypozyczeniaFilmu2.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM sakila3_7.payment;
