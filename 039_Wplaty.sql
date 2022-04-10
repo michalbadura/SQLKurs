c6fa148331c663bce62914c25e69f16768cb20cb
Michal Badura <mbadura@interia.eu>
2022-04-10 23:35:11 +0200

    39.1

--- 039_Wplaty.sql
+++ 039_Wplaty.sql
@@ -8,3 +8,22 @@ całkowita suma wpłat.
 Dodatkowo zadbaj o logiczne wyświetlenie wyniku używając ORDER BY.
  */
 
+select sum(amount), case
+        when amount < 2 then 'fee'
+        when amount >=2 then 'regular'
+        end as payment_type
+from payment
+    group by payment_type;
+
+
+
+
+select sum(amount), if (amount < 2, 'fee', 'regular') as payment_type
+from payment
+    group by payment_type;
+
+with payment_types as (
+    select sum(amount) as total_amount, if(amount < 2, 'fee', 'regular') as payment_type
+    from payment
+    group by payment_type
+) select total_amount, payment_type, total_amount / sum(total_amount) over () from payment_types;
